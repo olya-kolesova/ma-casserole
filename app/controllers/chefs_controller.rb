0f2e75f112b7a6e171c0240780864f09477be8f9
Olga Kolesova <olka.kolesova@gmail.com>
2021-08-24 16:13:46 +0100

    created index and show methods and their views

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,2 +1,15 @@
 class ChefsController < ApplicationController
+  def index
+    @chefs = Chef.all
+  end
+
+  def show
+    @chef = Chef.find([:id])
+  end
+
+  def full_name
+    @user = User.find[:user_id]
+    full_name = "#{user.first_name.capitalize} #{user.last_name.capitalize}"
+    return full_name
+  end
 end
