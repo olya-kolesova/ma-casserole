1074e95280956f4526182358cdfd5d992a5119af
Olga Kolesova <olka.kolesova@gmail.com>
2021-08-25 11:25:06 +0100

    added chef and user in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Chef.destroy_all
+
+puts "Creating user"
+
+user1 = User.create!(
+  email: "bobby@gmail.com",
+  password: "123456",
+  first_name: "Bobby",
+  last_name: "Smith"
+)
+
+puts "Creating chef"
+chef1 = Chef.create!(
+  speciality: "french",
+  experience: "very experienced",
+  user: user1
+)
