ba1fd11888f8009ea26e3adade3049311197309e
Aaron Battman <aaron.k.battman@outlook.com>
2021-08-25 12:06:47 +0100

    edit/upd. routes and meths only added. View to add later.

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,12 +1,11 @@
 class BookingsController < ApplicationController
+  before_action :dry_id_booking, only: [:show, :edit, :update]
 
   def index 
     @bookings = Booking.all
   end
 
-  def show
-    @booking = Booking.find(params[:id])
-  end
+  def show; end
 
   def new
     @booking = Booking.new
@@ -18,9 +17,23 @@ class BookingsController < ApplicationController
     # redirect_to booking_path(@booking)
   end
 
+  # def edit; end
+
+  # def update
+  #   @chef.find(params[:id])
+  #   @booking.update(booking_params)
+  #   booking.chef = @chefbooking.user = current_user
+  #   redirect_to booking_path(@booking)
+  #   redirect_to : root
+  # end
+
     private
 
   def booking_params
     params.require(:booking).permit(:user_id, :chef_id, :start_date, :end_date)
+  end
+
+  def dry_id_booking
+    @task = Booking.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :chefs, only: [:index, :show, :new, :create] do
-    resources  :bookings, only: [:index, :show,:new, :create]
+    resources  :bookings, only: [:index, :show, :new, :create, :edit, :update]
   end  
 
 end
