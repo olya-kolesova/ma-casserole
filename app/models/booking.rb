c737d5ee3059f04eae7dfc8b11d0b4d6a769802d
Aaron Battman <aaron.k.battman@outlook.com>
2021-08-26 13:42:16 +0100

    booking model vlds. - pres',end>start, psedo availability.

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :chef
+
+  validates :start_date, :end_date, presence: true  # , availability: true
+  validate :end_date_after_start_date
 end
