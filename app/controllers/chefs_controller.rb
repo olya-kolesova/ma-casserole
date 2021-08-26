e51112dd6fa61f2f214d7601a4a02b490b55da29
Olga Kolesova <olka.kolesova@gmail.com>
2021-08-26 22:57:35 +0100

    finished map styling

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -10,7 +10,9 @@ class ChefsController < ApplicationController
     @markers = @chefs.geocoded.map do |chef|
       {
         lat: chef.latitude,
-        lng: chef.longitude
+        lng: chef.longitude,
+        info_window: render_to_string(partial: "info_window", locals: { chef: chef }),
+        image_url: helpers.asset_url('lemon.png')
       }
     end
   end
