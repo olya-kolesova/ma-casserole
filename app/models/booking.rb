1b04f2f2dbd4f7dd1c3914dfb684c4a46d44f914
Aaron Battman <aaron.k.battman@outlook.com>
2021-08-26 13:46:46 +0100

    TYPO CORRECTION booking model vlds. - pres',end>start, psedo availability.

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,6 +2,6 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :chef
 
-  validates :start_date, :end_date, presence: true  # , availability: true
+  validate :start_date, :end_date, presence: true  # , availability: true
   validate :end_date_after_start_date
 end
